61e96c810ef91d84273d2f00ba3eddcfd460b54c
Hernan Velasquez Meriño <hernan.velasquez@sofka.local>
2022-10-28 23:02:34 -0500

    Primera parte de la pokeApi terminada

---
+++ README.md
@@ -0,0 +1,22 @@
+<p align="center">
+  <a href="http://nestjs.com/" target="blank"><img src="https://nestjs.com/img/logo-small.svg" width="200" alt="Nest Logo" /></a>
+</p>
+
+# Ejecutar en Desarrollo
+1. Clonar el Repositorio
+2. Ejecutar
+```
+$ npm install
+```
+3. Tener Nest CLI instalado
+```
+$ npm i -g @nestjs/cli
+```
+4. Levantar la base de datos (Verificar que Docker este ejecutando en segundo plano)
+```
+$ docker compose up -d
+```
+
+## Stack Usado
+* MongoDB
+* Nest
---
+++ src/pokemon/pokemon.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { PokemonService } from './pokemon.service';
+import { PokemonController } from './pokemon.controller';
+import { Pokemon, PokemonScheme } from './entities/pokemon.entity';
+
+@Module({
+  controllers: [PokemonController],
+  providers: [PokemonService],
+  imports: [
+    MongooseModule.forFeature([{ name: Pokemon.name, schema: PokemonScheme }]),
+  ],
+})
+export class PokemonModule {}
